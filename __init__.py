22679cb0b4c4a8b70cf66ffec56cbd5ac84c1380
Arius-Cr <arius-cr@qq.com>
2023-07-16 20:28:53 +0800

    修改版本号

--- __init__.py
+++ __init__.py
@@ -11,9 +11,9 @@ bl_info = {
     'description': "修复 Blender 中和 IME 相关的一些问题",
     'blender': (3, 0, 0),
     'version': (2, 0, 0),
-    '_pre_release': 'beta.4',
+    '_pre_release': 'beta.5',
     'location': "",
-    'warning': "beta.4（测试版）",
+    'warning': "beta.5（测试版）",
     'category': 'User Interface',
 }
 
